57bc69f836cce4967fec1c7ae41560a30d172c18
Fernando Palluzzi <51379764+fernandoPalluzzi@users.noreply.github.com>
2021-12-23 18:35:16 +0100

    Update vhCoverage.R

--- vhCoverage.R
+++ vhCoverage.R
@@ -1,4 +1,26 @@
-# VarHound - TSO500 - Coverage diagnostics
+#  VarHound - Coverage diagnostics
+
+#  Copyright (C) 2021 Fernando Palluzzi
+#  e-mail: <fernando.palluzzi@gmail.com>
+#  Bioinformatics facility,
+#  Gemelli Science and Technological Park (GSTeP),
+#  Fondazione Policlinico Universitario Agostino Gemelli IRCCS,
+#  Largo Agostino Gemelli 8, 00168 Rome, Italy
+
+#  VarHound is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  VarHound is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# -------------------------------------------------------------------- #
 
 library(dplyr)
 library(ggplot2)
